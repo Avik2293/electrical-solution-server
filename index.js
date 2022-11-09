5bff7e6bf1ec1e9914ac95619fd5f059602e9587
Avik2293 <aviksarker22@gmail.com>
2022-11-09 18:29:15 +0600

    reviews add and get, delete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,56 +16,110 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
-async function run(){
-    try{
-        const serviceCollection = client.db('electricalSolution').collection('services')
+async function run() {
+    try {
+        const serviceCollection = client.db('electricalSolution').collection('services');
 
-        app.get('/', async (req, res) =>{
+        const reviewCollection = client.db('electricalSolution').collection('reviews');
+
+        app.get('/', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.limit(3).toArray();
             res.send(services);
         });
 
-        app.get('/services', async (req, res) =>{
+        app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
 
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const cursor = serviceCollection.findOne(query);
+            const eachService = await cursor;
+            res.send(eachService);
+        });
+
+        //Add Service
+        
+
+        //Reviews
+        app.get('/myreviews', async (req, res) => {
+            let query = {}
+
+            if (req.query.reveiwerEmail) {
+                query = {
+                    reveiwerEmail: req.query.reveiwerEmail
+                }
+            }
+
+            const cursor = reviewCollection.find(query);
+            const myReviews = await cursor.toArray();
+            res.send(myReviews);
+        });
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        });
+
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        app.patch('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const update = req.body.update;
+            const query = { _id: ObjectId(id) };
+            const updatedDoc ={
+                $set: {
+                    review: update
+                }
+            }
+            const result = await reviewCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        });
+
 
         // app.get('/services', async(req, res) =>{
-            // const page = parseInt(req.query.page);
-            // const size = parseInt(req.query.size);
-//             console.log(page, size);
-            // const query = {}
-            // const cursor = serviceCollection.find(query);
-            // const products = await cursor.skip(page*size).limit(size).toArray();
-//             const count = await productCollection.estimatedDocumentCount();
-            // res.send({count, products});
+        // const page = parseInt(req.query.page);
+        // const size = parseInt(req.query.size);
+        //             console.log(page, size);
+        // const query = {}
+        // const cursor = serviceCollection.find(query);
+        // const products = await cursor.skip(page*size).limit(size).toArray();
+        //             const count = await productCollection.estimatedDocumentCount();
+        // res.send({count, products});
         // });
 
-//         app.post('/productsByIds', async(req, res) =>{
-//             const ids = req.body;
-//             const objectIds = ids.map(id => ObjectId(id))
-//             const query = {_id: {$in: objectIds}};
-//             const cursor = productCollection.find(query);
-//             const products = await cursor.toArray();
-//             res.send(products);
-//         })
+        //         app.post('/productsByIds', async(req, res) =>{
+        //             const ids = req.body;
+        //             const objectIds = ids.map(id => ObjectId(id))
+        //             const query = {_id: {$in: objectIds}};
+        //             const cursor = productCollection.find(query);
+        //             const products = await cursor.toArray();
+        //             res.send(products);
+        //         })
 
     }
-    finally{
+    finally {
 
     }
-} 
+}
 run().catch(err => console.error(err));
 
 // app.get('/', (req, res) =>{
 //     res.send('Electrical Solution server is running');
 // })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Electrical Solution running on: ${port}`)
 })
